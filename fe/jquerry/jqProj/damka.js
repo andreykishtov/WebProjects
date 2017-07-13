0c145e62148c642baf657fb13c67d4f5c0b5f1af
Andrey Kishtov <andreykishtov@gmail.com>
2017-07-13 14:26:08 +0300

    proj ver3

---
+++ fe/jquerry/jqProj/damka.js
@@ -0,0 +1,27 @@
+damka($("div"));
+
+
+function damka(div) {
+
+    var board = (function(params) {
+
+    })();
+
+    var userinterface = (function(params) {
+        addEventListener.apply(#startgame);
+
+        function startgame(params) {
+            1
+
+        }
+
+        return { "setElementsOnBoard": setElementsOnBoard "startGame": startGame }
+    })();
+
+
+    function logic(params) {
+
+    }
+
+
+}
--- fe/jquerry/jqProj/jqprojver3.js
+++ fe/jquerry/jqProj/jqprojver3.js
@@ -7,12 +7,24 @@ function shop(products, cartSelector) {
     var cart = (function(selector) {
         createTable(selector);
 
+        (function(element) {
+            var originalY = element.offset().top;
+            var topMargin = 20;
+            $(window).on('scroll', function(event) {
+                var scrollTop = $(window).scrollTop();
+                element.stop(false, false).animate({
+                    top: scrollTop < originalY ?
+                        0 : scrollTop - originalY + topMargin
+                }, 300);
+            });
+        })($("#" + cartSelector));
+
+
         function createTable(selector) {
 
             createRow("#" + selector, 1, '<th colspan="5">', ['Cart']);
             createRow("#" + selector, 5, '<th>', ['', 'Product', 'Quantity', 'Price', 'Total']); //id //number of cells in row //
             createRow("#" + selector, 5, '<td>', ['Total', "", '0', "", "0"], '<tr id="' + selector + '-end">');
-
         }
 
         function createRow(tableId, howmanyinrow, tdorth, data, tr) {
@@ -34,29 +46,36 @@ function shop(products, cartSelector) {
                 price = sku.price;
                 sku = sku.sku;
             }
-            //console.log($("#" + sku));
             if ($("#" + sku).length) {
                 AddToCreatedRow($("#" + sku), true)
+                totalChange("#" + selector + "-end");
             } else {
-                createRow("#" + selector + "-end", 5, '<td>', ['', title, '<input type="number" value="1">', price, price], "<tr id=" + sku + ">");
+                createRow("#" + selector + "-end", 5, '<td>', ['X', title, '<input type="number" value="1">', price, price], "<tr id=" + sku + ">");
+                $("#" + sku).find("td:nth-child(1)").click(function() {
+                    removeItem(sku);
+                });
+                totalChange("#" + selector + "-end");
+                $("#" + sku).change(function() {
+                    AddToCreatedRow($("#" + sku), false, selector);
+                });
             }
+
 
             function createRow(tableId, howmanyinrow, tdorth, data, tr) {
                 var tRow;
                 var tCell;
+                var cellindex = 0;
                 if (tr == undefined) {
                     tr = '<tr>';
                 }
                 $.each(data, function(i) {
                     if (!(i % howmanyinrow)) tRow = $(tr);
                     tCell = $(tdorth).html(data[i]);
+                    tCell.addClass(selector + "-" + cellindex++);
                     (tRow.append(tCell)).insertBefore($(tableId));
                 });
             }
-            itemSum(sku);
-            $("#" + sku).change(function() {
-                AddToCreatedRow($("#" + sku));
-            });
+
         }
 
         function AddToCreatedRow(skuId, add) { //id and check for add or onchange
@@ -67,19 +86,61 @@ function shop(products, cartSelector) {
                 skuId.find("td:nth-child(3) input").val(++howmany);
             }
             value = value.split("$");
-            save.text((value[1] * howmany).toFixed(2));
-            totalSum();
+            save.text("$" + (value[1] * howmany).toFixed(2));
+            totalChange("#" + selector + "-end");
         }
 
-        function itemSum(rowId) {
+        return { "addItem": addItem, "AddToCreatedRow": AddToCreatedRow };
 
+        function totalChange(total) {
+            var allcellsInput = $("." + selector + "-2" + " input");
+            var allcellsPrice = $("." + selector + "-3");
+            var totalObj = { count: [], price: [], total: [] };
+            allcellsInput.each(function(index, value) {
+                // console.log(index + ':' + $(value).val());
+                totalObj.count[index] = $(value).val();
+            });
+            allcellsPrice.each(function(index, value) {
+                totalObj.price[index] = removeDOllar($(value).text());
+            });
+            $(total + " td").each(function(index, value) {
+                if (index == 2) {
+                    $(value).text(calcTotal(totalObj.count).toFixed(2))
+                }
+                // $(value).text(totalObj.price[index]);
+                if (index == 4) {
+                    $(value).text("$" + calcTotalWithPrice(totalObj.count, totalObj.price).toFixed(2))
+                }
+            });
+
+            function removeDOllar(params) {
+                params = params.split("$");
+                return params[1];
+            }
+
+            function calcTotal(objcount) {
+                var total = 0;
+                for (var ind = 0; ind < objcount.length; ++ind) {
+                    total += parseInt(objcount[ind]);
+                }
+                return total;
+            }
+
+            function calcTotalWithPrice(objcount, objprice) {
+                var total = 0;
+                for (var ind = 0; ind < objcount.length; ++ind) {
+                    total += objcount[ind] * objprice[ind];
+                }
+                return total;
+            }
         }
 
-        function totalSum(nameofClass, rowId) {
-
+        function removeItem(sku) {
+            $("#" + sku).fadeOut("slow", function() {
+                $("#" + sku).remove();
+                totalChange("#" + selector + "-end");
+            });
         }
-
-        return { "addItem": addItem };
     })(cartSelector);
 
     for (var i = 0; i < products.length; ++i) {
@@ -89,30 +150,29 @@ function shop(products, cartSelector) {
 }
 
 function ProductList(items, selector, cart) {
-    if (items.sku) {
-        function createsku() {
-            for (var key in object) {
-                var element = object[key];
 
-            }
+    var container = $(selector);
+    items.forEach(function(element, i) {
+        if (!element.sku) {
+            element.sku = getRandomArbitrary(1111111111111111, 9999999999999999);
         }
-    }
-}
-var container = $(selector);
-items.forEach(function(element, i) {
-    var newDiv = $('<div>');
-    $('<img/>', { class: 'imageCart', src: element.image, width: 100 }).appendTo(newDiv);
-    $("<h2>", { class: 'NameOfItem', }).text(element.title).appendTo(newDiv);
-    $("<p>", { class: 'pricePlaceHolder', }).text(element.price).appendTo(newDiv);
-    var btn = $('<button>', { class: 'buttonCart', }).text('Add to Cart');
-    btn.data('prod', {
-        sku: element.sku,
-        title: element.title,
-        price: element.price
+        var newDiv = $('<div>');
+        $('<img/>', { class: 'imageCart', src: element.image, width: 100 }).appendTo(newDiv);
+        $("<h2>", { class: 'NameOfItem', }).text(element.title).appendTo(newDiv);
+        $("<p>", { class: 'pricePlaceHolder', }).text(element.price).appendTo(newDiv);
+        var btn = $('<button>', { class: 'buttonCart', }).text('Add to Cart');
+        btn.data('prod', {
+            sku: element.sku,
+            title: element.title,
+            price: element.price
+        });
+        btn.click(function() {
+            cart.addItem($(this).data('prod'));
+        }).appendTo(newDiv);
+        newDiv.appendTo($("#" + selector));
     });
-    btn.click(function() {
-        cart.addItem($(this).data('prod'));
-    }).appendTo(newDiv);
-    newDiv.appendTo($("#" + selector));
-});
+
+    function getRandomArbitrary(min, max) {
+        return Math.floor(Math.random() * (max - min) + min);
+    }
 }
