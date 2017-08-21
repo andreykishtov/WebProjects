d8e4877703728f87b7ba143b33753e37bf5634f6
Andrey Kishtov <andreykishtov@gmail.com>
2017-08-21 09:46:50 +0300

    after final

--- battleShip/battleshipver2/models/gamelogics.js
+++ battleShip/battleshipver2/models/gamelogics.js
@@ -91,7 +91,7 @@ class Game {
 
     gameEnds(IfHit, socketid) {
         if (IfHit) {
-            return (++this.winners[socketid] === 1) //no more ships
+            return (++this.winners[socketid] === 20) //no more ships
         }
     }
 }
