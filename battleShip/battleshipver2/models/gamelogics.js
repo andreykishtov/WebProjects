8f090693696c7cb13d0f1fbd2f2430b24641e4f0
Andrey Kishtov <andreykishtov@gmail.com>
2017-11-09 09:26:59 +0200

    readyness

--- battleShip/battleshipver2/models/gamelogics.js
+++ battleShip/battleshipver2/models/gamelogics.js
@@ -1,100 +1,85 @@
-var randomships = require('./randomShips');
-
 class Game {
-    constructor(id1, id2) {
-        this.player1 = id1;
-        this.player2 = id2;
-        this.gameInit();
-        this.currentPlayer;
-        //this.board1 = new randomships;
-        //this.board2 = new randomships;
-        this.winners = {};
-        this.winners[this.player1] = 0;
-        this.winners[this.player2] = 0;
-        // this.objOfPlayers[0].board = randomships.createShips(4);
-        //this.board1.printBoard();
-        // this.objOfPlayers[1].board = randomships.createShips(4);
-        //this.board2.printBoard();
-    }
+  constructor(id1, id2) {
+    this.player1 = id1;
+    this.player2 = id2;
+    this.gameInit();
+    this.currentPlayer;
+    this.winners = {};
+    this.winners[this.player1] = 0;
+    this.winners[this.player2] = 0;
+  }
 
-    gameInit() {
-        this.objOfPlayers = [{
-                id: this.player1,
-                board: {
-                    ship0: [10, 11, 12, 13],
-                    ship1: [17, 18, 19],
-                    ship2: [35, 25, 15],
-                    ship3: [31, 32],
-                    ship4: [44, 54],
-                    ship5: [51, 52],
-                    ship6: [81],
-                    ship7: [94],
-                    ship8: [79],
-                    ship9: [66]
-
-                }
-            },
-            {
-                id: this.player2,
-                board: {
-                    ship0: [10, 11, 12, 13],
-                    ship1: [7, 8, 9],
-                    ship2: [28, 38, 48],
-                    ship3: [31, 32],
-                    ship4: [44, 54],
-                    ship5: [51, 52],
-                    ship6: [80],
-                    ship7: [97],
-                    ship8: [78],
-                    ship9: [69]
-                }
-            }
-        ];
-    }
-
-    checkGame(socketid, cell) {
-        if (socketid != this.player1) { //uses board of other player!//
-            var currentboard = this.objOfPlayers[0].board;
-        } else {
-            var currentboard = this.objOfPlayers[1].board;
+  gameInit() {
+    this.objOfPlayers = [
+      {
+        id: this.player1,
+        board: {
+          ship0: [10, 11, 12, 13],
+          ship1: [17, 18, 19],
+          ship2: [35, 25, 15],
+          ship3: [31, 32],
+          ship4: [44, 54],
+          ship5: [51, 52],
+          ship6: [81],
+          ship7: [94],
+          ship8: [79],
+          ship9: [66]
         }
-        for (var key in currentboard) {
-            var ship = currentboard[key];
-            for (var index = 0; index < ship.length; index++) {
-                if (ship[index] == cell) {
-                    return true;
-                }
-            }
+      },
+      {
+        id: this.player2,
+        board: {
+          ship0: [10, 11, 12, 13],
+          ship1: [7, 8, 9],
+          ship2: [28, 38, 48],
+          ship3: [31, 32],
+          ship4: [44, 54],
+          ship5: [51, 52],
+          ship6: [80],
+          ship7: [97],
+          ship8: [78],
+          ship9: [69]
         }
-        return false;
-    }
+      }
+    ];
+  }
 
-    ifPlayerAllowed(socketid, ifHit) {
-        let otherPlayerID = this.FindOtherPlayer(socketid);
-        if (ifHit === true && otherPlayerID !== this.currentPlayer) {
-            return this.currentPlayer;
-        } else {
-            if (otherPlayerID !== this.currentPlayer) {
-                let player = this.currentPlayer;
-                this.currentPlayer = otherPlayerID;
-                return player;
-            }
-        }
-    }
+  checkGame(socketId, cell) {
+    var currentBoard = socketId != this.player1 ? this.objOfPlayers[0].board : this.objOfPlayers[1].board;
 
-    FindOtherPlayer(socketid, ishit) {
-        if (socketid === this.player1) { //uses board of other player!//
-            return this.player2;
-        } else {
-            return this.player1;
+    for (var key in currentBoard) {
+      var ship = currentBoard[key];
+      for (var index = 0; index < ship.length; index++) {
+        if (ship[index] == cell) {
+          return true;
         }
+      }
     }
+    return false;
+  }
 
-    gameEnds(IfHit, socketid) {
-        if (IfHit) {
-            return (++this.winners[socketid] === 20) //no more ships
-        }
+  ifPlayerAllowed(socketId, ifHit) {
+    let otherPlayerID = this.findOtherPlayer(socketId);
+    if (ifHit && otherPlayerID !== this.currentPlayer) {
+      return this.currentPlayer;
+    } else {
+      if (otherPlayerID !== this.currentPlayer) {
+        let player = this.currentPlayer;
+        this.currentPlayer = otherPlayerID;
+        return player;
+      }
     }
+  }
+
+  findOtherPlayer(socketId, ishit) {
+    return socketId === this.player1 ? this.player2 : this.player1;
+  }
+
+  gameEnds(IfHit, socketId) {
+    if (IfHit) {
+      return ++this.winners[socketId] === 20; //no more ships
+    }
+  }
 }
 
 module.exports = Game;
--- battleShip/battleshipver2/models/users.js
+++ battleShip/battleshipver2/models/users.js
@@ -1,64 +1,64 @@
 class Users {
-    constructor() {
-        this.users = [];
-        this.socketid = [];
-        this.currentGame = [];
-    }
-    getUsers() {
-        let usersinGame = [];
-        for (var user of this.currentGame) {
-            usersinGame.push(this.findUserName(user.player1));
-            usersinGame.push(this.findUserName(user.player2));
-        }
-        return { users: this.users, usersinGame: usersinGame };
-    }
+  constructor() {
+    this.users = [];
+    this.socketid = [];
+    this.currentGame = [];
+  }
 
-    addUser(username, socketid) {
-        this.users.push(username);
-        this.socketid.push(socketid);
+  getUsers() {
+    let usersinGame = [];
+    for (var user of this.currentGame) {
+      usersinGame.push(this.findUserName(user.player1));
+      usersinGame.push(this.findUserName(user.player2));
     }
+    return { users: this.users, usersinGame: usersinGame };
+  }
 
-    removeUser(username) {
-        for (let index = 0; index < this.users.length; ++index) {
-            if (username === this.users[index]) {
-                this.users.splice(index, 1);
-                this.socketid.splice(index, 1);
-            }
-        }
-    }
+  addUser(username, socketid) {
+    this.users.push(username);
+    this.socketid.push(socketid);
+  }
 
-    checkUser(username, socketid) {
-        for (let index = 0; index < this.users.length; ++index) {
-            if (username === this.users[index]) {
-                return true;
-            }
-        }
-        this.addUser(username, socketid);
+  removeUser(username) {
+    for (let index = 0; index < this.users.length; ++index) {
+      if (username === this.users[index]) {
+        this.users.splice(index, 1);
+        this.socketid.splice(index, 1);
+      }
     }
+  }
 
-    findsocketID(username) {
-        for (let index = 0; index < this.users.length; ++index) {
-            if (username === this.users[index]) {
-                return this.socketid[index];
-            }
-        }
+  checkUser(username, socketid) {
+    for (let index = 0; index < this.users.length; ++index) {
+      if (username === this.users[index]) {
+        return true;
+      }
     }
+    this.addUser(username, socketid);
+  }
 
-    findUserName(socketid) {
-        for (let index = 0; index < this.socketid.length; ++index) {
-            if (socketid === this.socketid[index]) {
-                return this.users[index];
-            }
+  findsocketID(username) {
+    for (let index = 0; index < this.users.length; ++index) {
+      if (username === this.users[index]) {
+        return this.socketid[index];
+      }
+    }
+  }
 
-        }
+  findUserName(socketid) {
+    for (let index = 0; index < this.socketid.length; ++index) {
+      if (socketid === this.socketid[index]) {
+        return this.users[index];
+      }
     }
-    findCurrentGame(socketid) {
-        for (let index = 0; index < this.currentGame.length; ++index) {
-            if (socketid === this.currentGame[index].player1 || socketid === this.currentGame[index].player2) {
-                return this.currentGame[index];
-            }
-        }
+  }
+  findCurrentGame(socketid) {
+    for (let index = 0; index < this.currentGame.length; ++index) {
+      if (socketid === this.currentGame[index].player1 || socketid === this.currentGame[index].player2) {
+        return this.currentGame[index];
+      }
     }
+  }
 }
 
-module.exports = new Users;
+module.exports = new Users();
